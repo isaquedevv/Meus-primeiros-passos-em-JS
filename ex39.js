1fbdd66ff771b419c90d9182851b30da8e735f5f
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-04-29 10:51:32 -0300

    Concluído Exercício 39: Múltiplo de 3 e 5

--- ex39.js
+++ ex39.js
@@ -12,10 +12,10 @@ if(temp==0){
     temp = v1 % 5
 
     if(temp==0){
-        console.log(`${v1} é divisível por 3 e 5`);
+        console.log(`${v1}`);
     }else{
-        console.log(`${v1} não é divisível por 3 e 5`);
+        
     }
 }else{
-    console.log(`${v1} não é divisível por 3 e 5`);
+
 }
