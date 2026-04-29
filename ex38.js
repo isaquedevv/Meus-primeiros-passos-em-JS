cf3d4a52433458d225cc32edaf5291dd1327aeee
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-04-29 10:52:23 -0300

    Concluído Exercício 38: Verificação de Valor Menor ou Igual a 3

--- ex38.js
+++ ex38.js
@@ -5,7 +5,7 @@ console.log("--- Verificação de Valor Menor ou Igual a 3 ---");
 let v1 = Number(prompt("Insira um valor numérico: "));
 
 if(v1<=3){
-    console.log(`Seu número é menor ou igual a 3`);
+    console.log(`${v1}`);
 }else{
-    console.log(`Seu número não é menor ou igual a 3`);
+
 }
