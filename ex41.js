3a1c1a9acb7ef20d5c5e06f179f387629c6ccb7d
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-04-29 10:49:41 -0300

    Concluído Exercício 41: Multiplicação e Verificação

---
+++ ex41.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- Multiplicação e Verificação ---");
+
+let v1 = Number(prompt("Insira o primeiro valor numérico: "));
+let temp
+
+temp = v1 * 2
+
+if(temp>30){
+    console.log(`${v1}`);
+}else{
+
+}
