f55a60086043f32fd9f85120e9d909f4c756b9ca
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-04-29 14:15:21 -0300

    Concluído Exercício 47: Valores Divisíveis por 4 Menores que 200

---
+++ ex47.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- Valores Divisíveis por 4 Menores que 200 ---");
+
+for (let numero = 4; numero <= 196; numero += 4) {
+    console.log(`${numero}`)
+}
