ee35a8d8aeb1ddc938b2b927fd778081580f75db
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-04-29 10:50:25 -0300

    Concluído Exercício 40: Soma e Verificação

--- ex40.js
+++ ex40.js
@@ -5,14 +5,13 @@ console.log("--- Soma e Verificação ---");
 let v1 = Number(prompt("Insira o primeiro valor numérico: "));
 let v2 = Number(prompt("Insira o segundo valor numérico: "));
 let v3 = Number(prompt("Insira o terceiro valor numérico: "));
+
 let temp
 
 temp = v1 + v2 + v3
 
 if(temp>100){
-    console.log(`A soma de ${v1}, ${v2} e ${v3} é maior que 100`);
-}else if(temp==100){
-    console.log(`A soma de ${v1}, ${v2} e ${v3} é igual a 100`);
+    console.log(`${v1}`);
 }else{
-    console.log(`A soma de ${v1}, ${v2} e ${v3} é menor que 100`);
+
 }
