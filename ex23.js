49466359405b011263aefe09b00bef50f47a0a31
isaquedevv <isaque.cordeiro@edu.senai.br>
2026-05-08 09:17:37 -0300

    (Alteração do commit) Concluído Exercício 23: Conversão de Pés para Metros

--- ex23.js
+++ ex23.js
@@ -4,7 +4,6 @@ console.log("--- Conversão de Pés para Metros ---");
 
 let pes = Number(prompt("Insira uma distância em Pés(Medida): "));
 
-
 let metros = pes * 0.3048
 
 console.log(`A distância em Metros é de: ${metros}`);
